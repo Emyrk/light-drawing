d2d61854be0d8d050860263f0d8240a4c6a2e8f4
Josh Deichmann <jxd8266@rit.edu>
2019-04-15 12:41:24 -0400

    Add smoothing to drawing

--- DrawingEngine.py
+++ DrawingEngine.py
@@ -2,6 +2,7 @@ from DebugUtils import display_all_img
 import cv2
 import numpy as np
 import math
+import pandas as pd
 
 
 class DrawingEngine:
@@ -23,22 +24,31 @@ class DrawingEngine:
 
     @staticmethod
     def _draw_lines(img, points, color):
+        points = DrawingEngine._smooth(points)
         if len(points) == 0:
             return
-        last_point = points[0]
         for i in range(1, len(points)):
             if points[i - 1] is None or points[i] is None:
                 continue
-            x1 = last_point[0]
-            x2 = points[i][0]
-            y1 = last_point[1]
-            y2 = points[i][1]
-            pythag = math.sqrt(pow(abs(x1-x2), 2) + pow(abs(y1-y2), 2))
+            DrawingEngine._draw_line(img, points[i-1], points[i], color)
 
-            # Only draw if at least 10 pixels away from last point
-            if (pythag > 10):
-                DrawingEngine._draw_line(img, last_point, points[i], color)
-                last_point = points[i]
+    @staticmethod
+    def _smooth(points):
+        size = 5
+        if len(points) < size:
+            return points
+        unzipped = list(zip(*points))
+        xs = list(unzipped[0])
+        ys = list(unzipped[1])
+        xs = pd.DataFrame(xs).rolling(size, min_periods=1).mean()
+        xs = np.array((xs[xs.columns[0]]))
+        ys = pd.DataFrame(ys).rolling(size, min_periods=1).mean()
+        ys = np.array((ys[ys.columns[0]]))
+        xs = xs.astype(int)
+        ys = ys.astype(int)
+
+        return list(zip(xs, ys))
+
 
     @staticmethod
     def _blank_img_of_size(img_wid, img_hei):
