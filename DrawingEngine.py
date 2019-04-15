b8ca86e1f846d9f52889b7cb4372cdbf8030b902
Josh Deichmann <jxd8266@rit.edu>
2019-04-14 21:31:30 -0400

    artificial smoothing (only draw if 10 pixels away from last point)

--- DrawingEngine.py
+++ DrawingEngine.py
@@ -1,6 +1,7 @@
 from DebugUtils import display_all_img
 import cv2
 import numpy as np
+import math
 
 
 class DrawingEngine:
@@ -24,10 +25,20 @@ class DrawingEngine:
     def _draw_lines(img, points, color):
         if len(points) == 0:
             return
+        last_point = points[0]
         for i in range(1, len(points)):
             if points[i - 1] is None or points[i] is None:
                 continue
-            DrawingEngine._draw_line(img, points[i - 1], points[i], color)
+            x1 = last_point[0]
+            x2 = points[i][0]
+            y1 = last_point[1]
+            y2 = points[i][1]
+            pythag = math.sqrt(pow(abs(x1-x2), 2) + pow(abs(y1-y2), 2))
+
+            # Only draw if at least 10 pixels away from last point
+            if (pythag > 10):
+                DrawingEngine._draw_line(img, last_point, points[i], color)
+                last_point = points[i]
 
     @staticmethod
     def _blank_img_of_size(img_wid, img_hei):
